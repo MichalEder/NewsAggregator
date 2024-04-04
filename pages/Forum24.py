5b931c54bd46f8e6804cd61dc77cdc8b52d24431
MichalEder <edermichal.eder@gmail.com>
2024-04-04 10:09:03 +0200

    edited forum24 web display

--- pages/Forum24.py
+++ pages/Forum24.py
@@ -3,20 +3,25 @@ from functions.database_interactions import read_resource_for_web
 
 st.set_page_config(layout="wide")
 
+@st.cache
+def load_data():
+    zahranicni = read_resource_for_web('zahraničí', 'forum24')
+    domaci = read_resource_for_web('domácí', 'forum24')
+    return zahranicni, domaci
 
-zahranicni = read_resource_for_web('zahraničí', 'forum24')
-domaci = read_resource_for_web('domácí', 'forum24')
+def display_article(article):
+    st.subheader(article[1])
+    st.markdown(f"**[Link]({article[5]})** - {article[3]}")
+    st.write(f"{article[4]}")
+
+zahranicni, domaci = load_data()
 
 st.title('Forum24.cz')
 
 
 with st.expander('Zahraniční'):
-    for articel in zahranicni:
-        st.subheader(articel[1])
-        st.markdown(f"[Link]({articel[5]}) {articel[3]}")
-        st.write(f"{articel[4]}")
+    for article in zahranicni:
+        display_article(article)
 with st.expander('Domácí'):
-    for articel in domaci:
-        st.subheader(articel[1])
-        st.markdown(f"[Link]({articel[5]}) {articel[3]}")
-        st.write(f"{articel[4]}")
+    for article in domaci:
+        display_article(article)
