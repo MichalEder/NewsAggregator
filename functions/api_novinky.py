453fe0adaa02edafef3f4fd6c91fa95d511278d9
MichalEder <edermichal.eder@gmail.com>
2024-04-04 08:31:44 +0200

    added NovinkyAPI data extraction

---
+++ functions/api_novinky.py
@@ -0,0 +1,42 @@
+import requests
+from functions.database_interactions import add_entry, read_resource
+
+
+def process_novinky_data(url='https://api-web.novinky.cz/v1/timelines'):
+    """Fetches data from Novinky.cz API, processes it, and adds new entries to the database."""
+
+    response = requests.get(url)
+    content = response.json()
+    original_data = {line[1] for line in read_resource('novinky')}
+
+    try:
+        for timeline in content['_items']:
+            for document in timeline['documents']['_items']:
+                title = document['title']
+                perex = document['perex']
+                uid = document['uid']
+                slug = document['slug']
+                section = slug.split('-')[0]
+                date = document['dateOfPublication']
+                url = f'https://www.novinky.cz/clanek/{slug}-{uid}'
+
+                data_local = {
+                    'Title': title,
+                    'Section': section,
+                    'Perex': perex,
+                    'url': url,
+                    'Date': date
+                }
+
+                if (data_local['Title'] not in original_data and
+                        data_local['Section'] in ['zahranicni', 'domaci', 'krimi', 'ekonomika']):
+                    original_data.add(data_local['Title'])
+                    add_entry(data_local, 'novinky')
+
+    except IndexError:
+        pass  # Handle potential errors in data structure
+
+
+if __name__ == "__main__":
+    novinky_url = "https://api-web.novinky.cz/v1/timelines"
+    process_novinky_data(novinky_url)
